962c51d18fbe9ba79bfddeb13fbb1cad417acbae
ssgzth <ssghadge8888@gmail.com>
2025-07-31 15:25:56 +0530

    created view functions and configured paths

--- Django1/firstApp/views.py
+++ Django1/firstApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Hello, world!, from the firstApp home view!")
+
+def about(request):
+    return HttpResponse("This is the about page of firstApp.")
 # Create your views here.
